47709a9a2980583e830f09cba1d44fde3eccb550
FatBoyLong <eugggggene@gmail.com>
2023-03-02 21:06:35 +0300

    added basic header markup

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 import classes from "./Header.module.css";
+import mealsImg from "../../assest/meals.jpg";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="A table of food" />
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Header;
